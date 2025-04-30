567d3e6d6cd7c395f0bf07bda8fb8a112c3b5f46
boxden <denbox030@gmail.com>
2025-04-30 17:33:59 +0300

    update + moviemaker lib

---
+++ Libraries/facepunch.moviemaker/Code/Assembly.cs
@@ -0,0 +1,6 @@
+global using System.Collections.Generic;
+global using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo( "moviemaker.editor" )]
+[assembly: InternalsVisibleTo( "moviemaker.unittest" )]
